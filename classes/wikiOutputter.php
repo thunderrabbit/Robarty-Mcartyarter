236e537e2d95f3748507a44e4240e81eaa78c40f
Rob Nugen <arty.robnugen.com@robnugen.com>
2019-12-17 21:15:31 -0800

    First draft can create wiki for an art page
    like this one https://wiki.robnugen.com/wiki/Art:Linking_Number_-_2010

---
+++ classes/wikiOutputter.php
@@ -0,0 +1,39 @@
+<?php
+/*
+	This file is part of Robarty Mcartyarter
+	Copyright (C) 2019 Rob Nugen
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+class wikiOutputter
+{
+    function output_art_page(string $filename, int $year)
+    {
+        echo nl2br(htmlspecialchars("
+        [[File:$filename, $year.jpg|900px|$filename, $year]]
+        [[File:$filename, back.jpg|thumb|$filename back]]
+        
+        An experiment in how many times lines could pass under and over one another, I started this in approximately $year.
+        
+        <permalink/>
+        
+        [[Category:Marker]]
+        [[Category:Shikishi]]
+        [[Category:24cm x 27cm]]
+        [[Category:$year]]
+        [[Category:art_pages]]
+        "));
+    }
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/*
+	This file is part of Robarty Mcartyarter
+	Copyright (C) 2019 Rob Nugen
+	
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+	
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+	
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+include "classes/wikiOutputter.php";
+
+$wO = new wikiOutputter();
+
+$wO->output_art_page("Lincoln Number", 2019);
